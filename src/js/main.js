7fd35dbd4ad919b52812647c15178ca81700de19
NikitaGulis <limonische@gmail.com>
2019-01-12 00:06:56 +0300

    Update defaults for main.js

--- src/js/main.js
+++ src/js/main.js
@@ -9,19 +9,15 @@ import objectFitImages from 'object-fit-images';
 // Модули
 import lazyLoad from './imports/lazyLoad';
 
-document.addEventListener('DOMContentLoaded', () => {
-    // После загрузки DOM
-    objectFitImages();
+// Object-fit для браузеров без его поддержки
+objectFitImages();
 
-    window.addEventListener('load', () => {
-        // После загрузки страницы
+window.addEventListener('load', () => {
+    // Регистрация Service Worker
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('service-worker.js');
+    }
 
-        // Ленивая загрузка изображений
-        lazyLoad();
-
-        // Регистрация Service Worker
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('service-worker.js');
-        }
-    });
+    // Ленивая загрузка изображений
+    lazyLoad();
 });
