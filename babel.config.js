c80424dcaae70cfb4a53763d3541a99eba9450ec
NikitaGulis <limonische@gmail.com>
2019-01-18 01:05:52 +0300

    Replace .babelrc with babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+// Конфигурация Babel
+
+module.exports = api => {
+    api.cache(true);
+
+    const presets = [
+        [
+            '@babel/preset-env',
+            {
+                modules: false,
+                // Автоматическая иньекция только нужных полифилов
+                useBuiltIns: 'usage'
+            }
+        ]
+    ];
+    const plugins = [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-private-methods',
+        '@babel/plugin-syntax-dynamic-import'
+    ];
+
+    return {
+        presets,
+        plugins
+    };
+};
