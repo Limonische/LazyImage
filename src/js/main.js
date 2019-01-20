682853020c33c7f25011aee6d43674e062c209e9
NikitaGulis <limonische@gmail.com>
2019-01-21 00:37:10 +0300

    Add main tag to wrapper and comment default js code in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,26 +4,26 @@
 import '../sass/styles.sass';
 
 // Библиотеки
-import objectFitImages from 'object-fit-images';
+// import objectFitImages from 'object-fit-images';
 
 // Модули
 import lazyLoad from './imports/lazyLoad';
 
 // Object-fit для браузеров без его поддержки
-objectFitImages();
+// objectFitImages();
 
 window.addEventListener('load', () => {
     // Регистрация Service Worker
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('service-worker.js')
-            .then(registration => {
-                console.log('Service worker registered: ', registration);
-            })
-            .catch(registrationError => {
-                console.log('Service worker registration failed: ', registrationError);
-            });
-    }
+    // if ('serviceWorker' in navigator) {
+    //     navigator.serviceWorker
+    //         .register('service-worker.js')
+    //         .then(registration => {
+    //             console.log('Service worker registered: ', registration);
+    //         })
+    //         .catch(registrationError => {
+    //             console.log('Service worker registration failed: ', registrationError);
+    //         });
+    // }
 
     // Ленивая загрузка изображений
     lazyLoad();
