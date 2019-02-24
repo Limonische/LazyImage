b14e60aa772442510e81b11b89335cade18d67c9
NikitaGulis <limonische@gmail.com>
2019-02-24 19:01:09 +0300

    Change babel loader config to simpler one

--- webpack/js.js
+++ webpack/js.js
@@ -6,11 +6,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader'
-                    }
-                ]
+                use: ['babel-loader']
             }
         ]
     }
