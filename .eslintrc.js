fc1a7c6a5a5d68eea7b59500963c8ce069310c7b
Limonische <gulisnikita@gmail.com>
2019-03-10 18:43:28 +0300

    Remove node from env in eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,7 @@ module.exports = {
     parser: 'babel-eslint',
     extends: 'airbnb-base',
     env: {
-        browser: true,
-        node: true
+        browser: true
     },
     rules: {
         indent: ['error', 4],
