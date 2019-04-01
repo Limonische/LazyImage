c2f9eced7ae534ce526ea65cf0459cf05322cf30
Limonische <gulisnikita@gmail.com>
2019-04-02 00:38:57 +0300

    Remove commented out service worker registration

--- src/js/main.js
+++ src/js/main.js
@@ -12,17 +12,3 @@ import lazyLoadImages from './imports/lazyLoad';
 
 // Lazy loading for images
 lazyLoadImages();
-
-// window.addEventListener('load', () => {
-//     // Register Service Worker
-//     if ('serviceWorker' in navigator) {
-//         navigator.serviceWorker
-//             .register('service-worker.js')
-//             .then(registration => {
-//                 console.log('Service worker registered: ', registration);
-//             })
-//             .catch(registrationError => {
-//                 console.log('Service worker registration failed: ', registrationError);
-//             });
-//     }
-// });
