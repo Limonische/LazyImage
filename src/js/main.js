a9b051dcffbc783c3df1b73acc75a1f613daa9be
Limonische <gulisnikita@gmail.com>
2019-04-05 12:57:50 +0300

    Remove default modules and libraries from main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,5 @@
 import '../sass/styles.sass';
 
 // Libraries
-// import objectFitImages from 'object-fit-images';
 
 // Modules
-import lazyLoadImages from './imports/lazyLoad';
-
-// object-fit for browsers without support
-// objectFitImages();
-
-window.addEventListener('load', () => {
-    // Lazy loading for images
-    lazyLoadImages();
-});
