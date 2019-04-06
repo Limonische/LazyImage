2afb75571daad2335edf65021d63849618a47415
Limonische <gulisnikita@gmail.com>
2019-04-06 23:09:24 +0300

    Add comments to lazyLoadImages file

--- src/js/imports/lazyLoadImages.js
+++ src/js/imports/lazyLoadImages.js
@@ -2,6 +2,7 @@
 const lazyLoadImages = async () => {
     const lazyImages = [...document.querySelectorAll('img.lazy')];
 
+    // Load selected image
     const loadLazyImage = lazyImage => {
         const { src, srcset } = lazyImage.dataset;
 
@@ -10,12 +11,14 @@ const lazyLoadImages = async () => {
         lazyImage.classList.remove('lazy');
     };
 
+    // Check for IntersectionObserver support and load polyfill if needed
     if (!('IntersectionObserver' in window)
         || !('IntersectionObserverEntry' in window)
         || !('intersectionRatio' in window.IntersectionObserverEntry.prototype)) {
         await import(/* webpackChunkName: 'intersection-observer' */ 'intersection-observer');
     }
 
+    // Create observer for lazy images
     const lazyImageObserver = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
@@ -27,13 +30,16 @@ const lazyLoadImages = async () => {
         });
     });
 
+    // Check for requestIdleCallback support and load polyfill if needed
     if (!('requestIdleCallback' in window)) {
         await import(/* webpackChunkName: 'request-idle-callback' */ 'requestidlecallback-polyfill');
     }
 
     lazyImages.forEach(lazyImage => {
+        // Observe lazy images
         lazyImageObserver.observe(lazyImage);
 
+        // Load images when browser is free
         requestIdleCallback(() => {
             loadLazyImage(lazyImage);
             lazyImageObserver.unobserve(lazyImage);
