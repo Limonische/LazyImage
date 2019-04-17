2ea7ff4ee0b185e9a6f773f933f895611a76ca34
Limonische <gulisnikita@gmail.com>
2019-04-18 02:10:15 +0300

    Add lazyLoadImages function

--- src/js/imports/lazyLoadImages.js
+++ src/js/imports/lazyLoadImages.js
@@ -1,48 +1,29 @@
-// Lazy loading for images
 const lazyLoadImages = async () => {
-    const lazyImages = [...document.querySelectorAll('img.lazy')];
-
-    // Load selected image
-    const loadLazyImage = lazyImage => {
-        const { src, srcset } = lazyImage.dataset;
-
-        lazyImage.src = src || '';
-        lazyImage.srcset = srcset || '';
-        lazyImage.classList.remove('lazy');
-    };
-
-    // Check for IntersectionObserver support and load polyfill if needed
-    if (!('IntersectionObserver' in window)
+    if (
+        !('IntersectionObserver' in window)
         || !('IntersectionObserverEntry' in window)
-        || !('intersectionRatio' in window.IntersectionObserverEntry.prototype)) {
+        || !('intersectionRatio' in window.IntersectionObserverEntry.prototype)
+    ) {
         await import(/* webpackChunkName: 'intersection-observer' */ 'intersection-observer');
     }
 
-    // Create observer for lazy images
-    const lazyImageObserver = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const lazyImage = entry.target;
-
-                loadLazyImage(lazyImage);
-                lazyImageObserver.unobserve(lazyImage);
-            }
-        });
-    });
-
-    // Check for requestIdleCallback support and load polyfill if needed
     if (!('requestIdleCallback' in window)) {
         await import(/* webpackChunkName: 'request-idle-callback' */ 'requestidlecallback-polyfill');
     }
 
-    lazyImages.forEach(lazyImage => {
-        // Observe lazy images
-        lazyImageObserver.observe(lazyImage);
+    const io = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            entry.target.visible = entry.isIntersecting;
+        });
+    });
 
-        // Load images when browser is free
+    const lazyImages = [...document.querySelectorAll('lhm-lazy-image')];
+
+    lazyImages.forEach(image => {
+        io.observe(image);
+
         requestIdleCallback(() => {
-            loadLazyImage(lazyImage);
-            lazyImageObserver.unobserve(lazyImage);
+            image.visible = true;
         });
     });
 };
--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,11 @@ import '../sass/styles.sass';
 // Libraries
 
 // Modules
+import LazyImage from './imports/LazyImage';
+import lazyLoadImages from './imports/lazyLoadImages';
+
+customElements.define('lhm-lazy-image', LazyImage);
+
+window.addEventListener('load', () => {
+    lazyLoadImages();
+});
