b5d24efd0fd74ec2bafeffbdf7448a1d62fce166
Limonische <gulisnikita@gmail.com>
2019-04-18 01:57:38 +0300

    Add LazyImage element

---
+++ src/js/imports/LazyImage.js
@@ -0,0 +1,132 @@
+class LazyImage extends HTMLElement {
+    loaded = false;
+
+    firstSrc = true;
+
+    firstSrcset = true;
+
+    srcset = null;
+
+    src = null;
+
+    image = new Image();
+
+    wrapper = document.createElement('div');
+
+    static get observedAttributes() {
+        return ['srcset', 'src', 'sizes', 'alt', 'image-class', 'width', 'height', 'fit', 'position'];
+    }
+
+    constructor() {
+        super();
+
+        this.wrap();
+        this.addEventListeners();
+        this.addInitialStyles();
+    }
+
+    set visible(visible) {
+        if ((!this.srcset && !this.src) || !visible || this.loaded) return;
+
+        if (this.srcset) this.image.srcset = this.srcset;
+        if (this.src) this.image.src = this.src;
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch (name) {
+        case 'srcset':
+            if (oldValue === newValue) break;
+            if (this.firstSrcset) {
+                this.srcset = newValue;
+                this.loaded = false;
+                this.firstSrcset = false;
+            } else {
+                this.image.srcset = newValue;
+                this.loaded = false;
+                this.showWrapper();
+            }
+            break;
+        case 'src':
+            if (oldValue === newValue) break;
+            if (this.firstSrc) {
+                this.src = newValue;
+                this.loaded = false;
+                this.firstSrc = false;
+            } else {
+                this.image.src = newValue;
+                this.loaded = false;
+                this.showWrapper();
+            }
+            break;
+        case 'sizes':
+            this.image.sizes = newValue;
+            break;
+        case 'alt':
+            this.image.alt = newValue;
+            break;
+        case 'image-class':
+            this.image.className = newValue;
+        case 'width':
+            this.style.width = newValue;
+            break;
+        case 'height':
+            this.style.height = newValue;
+            break;
+        case 'fit':
+            this.style.objectFit = newValue;
+            break;
+        case 'position':
+            this.style.objectPosition = newValue;
+            break;
+        default:
+            break;
+        }
+    }
+
+    addEventListeners() {
+        this.image.addEventListener('load', this.onImageLoad);
+    }
+
+    onImageLoad = () => {
+        this.loaded = true;
+        this.appendChild(this.image);
+        this.hideWrapper();
+    }
+
+    wrap() {
+        this.wrapper.innerHTML = this.innerHTML;
+        this.innerHTML = '';
+        this.appendChild(this.wrapper);
+    }
+
+    addInitialStyles() {
+        this.style.display = 'block';
+        this.style.position = 'relative';
+
+        this.image.style.width = '100%';
+        this.image.style.height = '100%';
+        this.image.style.objectFit = 'inherit';
+        this.image.style.objectPosition = 'inherit';
+
+        this.wrapper.style.transition = 'opacity .5s ease-in-out';
+        this.wrapper.style.position = 'absolute';
+        this.wrapper.style.top = 0;
+        this.wrapper.style.right = 0;
+        this.wrapper.style.bottom = 0;
+        this.wrapper.style.left = 0;
+    }
+
+    hideWrapper() {
+        setTimeout(() => {
+            if (this.loaded) this.wrapper.style.opacity = 0;
+        }, 100);
+    }
+
+    showWrapper() {
+        setTimeout(() => {
+            if (!this.loaded) this.wrapper.style.opacity = 1;
+        }, 100);
+    }
+}
+
+export default LazyImage;
